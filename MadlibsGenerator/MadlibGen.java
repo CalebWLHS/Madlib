e495c4a4d426b55d79641928cae0868d488ecf6c
CalebWLHS <113708867+CalebWLHS@users.noreply.github.com>
2022-09-20 08:01:32 -0700

    Add files via upload

--- MadlibsGenerator/MadlibGen.java
+++ MadlibsGenerator/MadlibGen.java
@@ -64,9 +64,9 @@ public class MadlibGen
         String secondVerb = input.nextLine();
         System.out.print("Give me a third verb: ");
         String thirdVerb = input.nextLine();
-        System.out.print("Give me a seventh adject: ");
+        System.out.print("Give me a seventh adjective: ");
         String seventhAdject = input.nextLine();
-        System.out.println("Here is your madlib "+name+"   "+month+day+year);
-        System.out.print("It was a"+firstAdject+"summer day."+firstNoun+"and I were excited to go camping at"+location+". It was my first time going there. I packed my favorite"+clothing+". It is "+secondAdject+"and "+thirdAdject+". Perfect for camping! On the road we went in our "+fourthAdject+" "+fifthAdject+" van! We were listening to "+songTitle+" all the way down. The drive was about 5 hours but it was so worth it. When we got there we unpacked the van. I could smell"+food+" being cooked. It smelled "+sixthAdject+" . I "+firstPastTenseVerb+" to the room I was staying in with my "+secondNoun+". The next thing I knew, "+thirdNoun+" came and "+secondPastTenseVerb+" on the bed. I heard my mom scream, 'Get off the bed!' I "+thirdPastTenseVerb+" outside. I saw "+fourthNoun+". It was "+fifthNoun+". Over the next few days I got to "+firstVerb+", "+secondVerb+" ,and "+thirdVerb+". My camping trip was "+seventhAdject+".");
+        System.out.println("Here is your madlib "+name+" "+month+day+year);
+        System.out.print("It was a "+firstAdject+" summer day."+firstNoun+" and I were excited to go camping at "+location+". It was my first time going there. I packed my favorite "+clothing+". It is "+secondAdject+" and "+thirdAdject+". Perfect for camping! On the road we went in our "+fourthAdject+" "+fifthAdject+" van! We were listening to "+songTitle+" all the way down. The drive was about 5 hours but it was so worth it. When we got there we unpacked the van. I could smell "+food+" being cooked. It smelled "+sixthAdject+". I "+firstPastTenseVerb+" to the room I was staying in with my "+secondNoun+". The next thing I knew, "+thirdNoun+" came and "+secondPastTenseVerb+" on the bed. I heard my mom scream, 'Get off the bed!' I "+thirdPastTenseVerb+" outside. I saw "+fourthNoun+". It was "+fifthNoun+". Over the next few days I got to "+firstVerb+", "+secondVerb+" ,and "+thirdVerb+". My camping trip was "+seventhAdject+".");
     }
 }
